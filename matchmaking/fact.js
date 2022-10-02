dc4b803b014f2cb08cef6983ab92ae7233c98e9e
Vansh Dhiman <vanshdhiman86@gmail.com>
2022-10-02 10:29:00 +0530

    first

---
+++ matchmaking/fact.js
@@ -0,0 +1,15 @@
+facts = ["In its more than 13-billion-mile journey, object’s light becomes heavily shifted toward the red side of the electromagnetic spectrum.Therefore, JWST’s primary mirrors are plated in 24-karat gold.",
+        "Launching in 1990, Hubble Space Telescope was the largest space telescope in history. ",
+        "JWST will be four times farther from Earth than our Moon!",
+        "Sharpness of vision, or angular resolution, is everything for space telescopes. And, JWST is no slouch in that department.In fact, the telescope could clearly see a US penny from around 24 miles away.",
+        "JWST’s state-of-the-art infrared instruments will help us locate new planets and accurately identify the presence of important things, like water.",
+        "One Side of the Telescope Is Hotter Than Death Valley, the Other Side Is Colder Than Antarctica",
+        "JWST’s Science Instruments Will Function at Close To Absolute Zero",
+        "James Webb is 6.5 meters in diameter, made out of 18 different mirror segments, with a collecting area of 25.37 square meters.",
+        "James Webb’s mirrors are the lightest large telescope mirrors of all-time.",
+        "When it runs out of fuel, its fate will be to permanently reside in a “graveyard orbit” around the Sun."
+        ];
+var randomf = Math.floor(Math.random() * 10);
+document.getElementById("p1").innerHTML = facts[randomf];
+document.getElementById("p2").innerHTML = facts[randomf];
+randomf = Math.floor(Math.random() * 10);
